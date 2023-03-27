8febef8b960b4fea96cf19b93c04a61cd2d809d4
Bart Duisters <bartduisters@bartduisters.com>
2023-03-27 02:26:04 +0200

    fix: remove old theme import

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -10,15 +10,6 @@ function install(app: any) {
 import "./assets/main.css";
 import "./styles/tailwind.css";
 
-import "./themes/theme-vuetiful.css";
-// import "./themes/theme-modern.css";
-// import "./themes/theme-rocket.css";
-// import "./themes/theme-sahara.css";
-// import "./themes/theme-seafoam.css";
-// import "./themes/theme-seasonal.css";
-// import "./themes/theme-test.css";
-// import "./themes/theme-vintage.css";
-
 export default { install };
 
 export * from "./components";
