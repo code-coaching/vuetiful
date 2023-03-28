cdd072ca76d1a750fa7a9911eff0478b4e68de13
Bart Duisters <bartduisters@bartduisters.com>
2023-03-28 02:46:21 +0200

    feat: active side bar link item

--- docs/src/components/SideBarLink.vue
+++ docs/src/components/SideBarLink.vue
@@ -1,5 +1,11 @@
 <template>
-  <q-item clickable @click="onClick()" tag="a">
+  <q-item
+    :active="name === activeRoute"
+    active-class="active-item"
+    clickable
+    @click="onClick()"
+    tag="a"
+  >
     <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
@@ -13,59 +19,67 @@
   </q-item>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { useRouter } from 'vue-router';
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
-export default defineComponent({
-  props: {
-    title: {
-      type: String,
-      required: true,
-    },
-
-    caption: {
-      type: String,
-      default: '',
-    },
-
-    link: {
-      type: String,
-      default: '#',
-    },
-
-    name: {
-      type: String,
-      default: '',
-    },
-
-    icon: {
-      type: String,
-      default: '',
-    },
-
-    params: {
-      type: Object,
-      default: () => ({}),
-    },
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
   },
-  setup(props) {
-    const router = useRouter();
 
-    const onClick = () => {
-      if (props.link.split('http')[0] === '') {
-        window.open(props.link, '_blank')?.focus();
-      } else {
-        const { params } = props;
-        if (Object.keys(params).length) {
-          void router.push({ name: props.name, params });
-        } else {
-          void router.push({ name: props.name });
-        }
-      }
-    };
+  caption: {
+    type: String,
+    default: '',
+  },
 
-    return { onClick };
+  link: {
+    type: String,
+    default: '#',
+  },
+
+  name: {
+    type: String,
+    default: '',
+  },
+
+  icon: {
+    type: String,
+    default: '',
+  },
+
+  params: {
+    type: Object,
+    default: () => ({}),
   },
 });
+
+const router = useRouter();
+const route = useRoute();
+const activeRoute = computed(() => route.name);
+
+const onClick = () => {
+  if (props.link.split('http')[0] === '') {
+    window.open(props.link, '_blank')?.focus();
+  } else {
+    const { params } = props;
+    if (Object.keys(params).length) {
+      void router.push({ name: props.name, params });
+    } else {
+      void router.push({ name: props.name });
+    }
+  }
+};
 </script>
+
+<style scoped>
+.active-item {
+  background-color: rgb(var(--color-surface-300));
+  color: rgb(var(--on-surface));
+}
+
+.dark .active-item {
+  background-color: rgb(var(--color-surface-900));
+}
+</style>
