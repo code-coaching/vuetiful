62ff283bbe5dbf1ff3d1df871764b9e3058c4ef5
Bart Duisters <bartduisters@bartduisters.com>
2023-04-21 10:36:19 +0200

    fix: add 'it' to rail test

--- ui/src/services/rail.service.test.ts
+++ ui/src/services/rail.service.test.ts
@@ -1,11 +1,13 @@
-import { describe, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import { useRail } from "./rail.service";
 
 const { selectedRailTile } = useRail();
 
 describe("useRail", () => {
   describe("selectedRailTile", () => {
-    selectedRailTile.value = "John Duck";
-    expect(selectedRailTile.value).toBe("John Duck");
+    it("should expose selectedRailTile", () => {
+      selectedRailTile.value = "John Duck";
+      expect(selectedRailTile.value).toBe("John Duck");
+    });
   });
 });
