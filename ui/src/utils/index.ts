a5c5e529fb41b667631c5988ab2a783083febb66
Bart Duisters <bartduisters@bartduisters.com>
2023-04-24 11:57:17 +0200

    refactor: rename code-block to v-code-block

--- ui/src/utils/index.ts
+++ ui/src/utils/index.ts
@@ -1,7 +1,7 @@
-import CodeBlock from "./code-block/code-block.vue";
+import VCodeBlock from "./code-block/VCodeBlock.vue";
 import { useDarkMode } from "./dark-mode/dark-mode.service";
 import DarkModeSwitch from "./dark-mode/dark-mode.vue";
 import ThemeSwitcher from "./theme/theme-switcher.vue";
 import { useTheme } from "./theme/theme.service";
 
-export { DarkModeSwitch, ThemeSwitcher, useDarkMode, useTheme, CodeBlock };
+export { DarkModeSwitch, ThemeSwitcher, useDarkMode, useTheme, VCodeBlock };
