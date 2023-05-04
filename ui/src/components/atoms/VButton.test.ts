5a08245f3e1ad98abad9548553c68a4ebc7bf4d0
Bart Duisters <bartduisters@bartduisters.com>
2023-05-04 21:54:29 +0200

    refactor: rename wrapper

--- ui/src/components/atoms/VButton.test.ts
+++ ui/src/components/atoms/VButton.test.ts
@@ -7,36 +7,36 @@ test("VButton", () => {
 });
 
 test("VButton using slot", () => {
-  const vButtonElement = mount(VButton, {
+  const wrapper = mount(VButton, {
     slots: {
       default: "John Duck",
     },
   });
 
-  expect(vButtonElement.text()).toContain("John Duck");
+  expect(wrapper.text()).toContain("John Duck");
 });
 
 describe("VButton props", () => {
   describe("given icon is true", () => {
     test("should have btn-icon class, not have btn class", () => {
-      const vButtonElement = mount(VButton, {
+      const wrapper = mount(VButton, {
         props: {
           icon: true,
         },
       });
-      expect(vButtonElement.classes()).toContain("btn-icon");
-      expect(vButtonElement.classes()).not.toContain("btn");
+      expect(wrapper.classes()).toContain("btn-icon");
+      expect(wrapper.classes()).not.toContain("btn");
     });
   });
   describe("given icon is false", () => {
     test("should have btn class, not have btn-icon class", () => {
-      const vButtonElement = mount(VButton, {
+      const wrapper = mount(VButton, {
         props: {
           icon: false,
         },
       });
-      expect(vButtonElement.classes()).not.toContain("btn-icon");
-      expect(vButtonElement.classes()).toContain("btn");
+      expect(wrapper.classes()).not.toContain("btn-icon");
+      expect(wrapper.classes()).toContain("btn");
     });
   });
 });
@@ -44,13 +44,13 @@ describe("VButton props", () => {
 describe("VButton events", () => {
   describe("given click event", () => {
     test("should preventDefault and emit click event", async () => {
-      const vButtonElement = mount(VButton);
+      const wrapper = mount(VButton);
       const clickEvent = { preventDefault: () => {} };
       const preventDefaultSpy = vi.spyOn(clickEvent, "preventDefault");
-      vButtonElement.trigger("click", clickEvent);
-      await vButtonElement.vm.$nextTick();
+      wrapper.trigger("click", clickEvent);
+      await wrapper.vm.$nextTick();
       expect(preventDefaultSpy).toHaveBeenCalled();
-      expect(vButtonElement.emitted("click")).toBeTruthy();
+      expect(wrapper.emitted("click")).toBeTruthy();
     });
   });
 });
@@ -58,50 +58,50 @@ describe("VButton events", () => {
 describe("VButton a11y", () => {
   describe("a11y role", () => {
     test("should have role button", () => {
-      const vButtonElement = mount(VButton);
-      expect(vButtonElement.attributes("role")).toBe("button");
+      const wrapper = mount(VButton);
+      expect(wrapper.attributes("role")).toBe("button");
     });
   });
   describe("a11y tabindex", () => {
     test("should have tabindex 0", () => {
-      const vButtonElement = mount(VButton);
-      expect(vButtonElement.attributes("tabindex")).toBe("0");
+      const wrapper = mount(VButton);
+      expect(wrapper.attributes("tabindex")).toBe("0");
     });
   });
   describe("given keydown event", () => {
     describe("given key is Enter", () => {
       test("should preventDefault and emit click event", async () => {
-        const vButtonElement = mount(VButton);
+        const wrapper = mount(VButton);
         const keydownEvent = { key: "Enter", preventDefault: () => {} };
         const preventDefaultSpy = vi.spyOn(keydownEvent, "preventDefault");
-        vButtonElement.trigger("keydown", keydownEvent);
-        await vButtonElement.vm.$nextTick();
+        wrapper.trigger("keydown", keydownEvent);
+        await wrapper.vm.$nextTick();
         expect(preventDefaultSpy).toHaveBeenCalled();
-        expect(vButtonElement.emitted("click")).toBeTruthy();
+        expect(wrapper.emitted("click")).toBeTruthy();
       });
     });
 
     describe("given key is Space", () => {
       test("should preventDefault and not emit click event", async () => {
-        const vButtonElement = mount(VButton);
+        const wrapper = mount(VButton);
         const keydownEvent = { key: " ", preventDefault: () => {} };
         const preventDefaultSpy = vi.spyOn(keydownEvent, "preventDefault");
-        vButtonElement.trigger("keydown", keydownEvent);
-        await vButtonElement.vm.$nextTick();
+        wrapper.trigger("keydown", keydownEvent);
+        await wrapper.vm.$nextTick();
         expect(preventDefaultSpy).toHaveBeenCalled();
-        expect(vButtonElement.emitted("click")).toBeFalsy();
+        expect(wrapper.emitted("click")).toBeFalsy();
       });
     });
 
     describe("given key is not Enter or Space", () => {
       test("should not preventDefault and not emit click event", async () => {
-        const vButtonElement = mount(VButton);
+        const wrapper = mount(VButton);
         const keydownEvent = { key: "a", preventDefault: () => {} };
         const preventDefaultSpy = vi.spyOn(keydownEvent, "preventDefault");
-        vButtonElement.trigger("keydown", keydownEvent);
-        await vButtonElement.vm.$nextTick();
+        wrapper.trigger("keydown", keydownEvent);
+        await wrapper.vm.$nextTick();
         expect(preventDefaultSpy).not.toHaveBeenCalled();
-        expect(vButtonElement.emitted("click")).toBeFalsy();
+        expect(wrapper.emitted("click")).toBeFalsy();
       });
     });
   });
@@ -109,13 +109,13 @@ describe("VButton a11y", () => {
   describe("given keyup event", () => {
     describe("given key is Space", () => {
       test("should preventDefault and emit click event", async () => {
-        const vButtonElement = mount(VButton);
+        const wrapper = mount(VButton);
         const keyupEvent = { key: " ", preventDefault: () => {} };
         const preventDefaultSpy = vi.spyOn(keyupEvent, "preventDefault");
-        vButtonElement.trigger("keyup", keyupEvent);
-        await vButtonElement.vm.$nextTick();
+        wrapper.trigger("keyup", keyupEvent);
+        await wrapper.vm.$nextTick();
         expect(preventDefaultSpy).toHaveBeenCalled();
-        expect(vButtonElement.emitted("click")).toBeTruthy();
+        expect(wrapper.emitted("click")).toBeTruthy();
       });
     });
   });
