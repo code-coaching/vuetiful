f624212064b9e7adace0dde990bf3dfbd26ebc3b
Bart Duisters <bartduisters@bartduisters.com>
2023-06-18 19:18:18 +0200

    feat: VAccordion

---
+++ ui/src/components/molecules/VAccordion/VAccordion.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { provide } from 'vue';
+
+const props = defineProps({
+  hover: {
+    type: String,
+    default: 'hover:variant-soft',
+  },
+  background: {
+    type: String,
+    default: 'bg-surface-200-700-token',
+  },
+});
+
+provide('hover', props.hover);
+provide('background', props.background);
+</script>
+
+<template>
+  <div class="vuetiful-accordion flex w-full flex-col gap-1">
+    <slot></slot>
+  </div>
+</template>
